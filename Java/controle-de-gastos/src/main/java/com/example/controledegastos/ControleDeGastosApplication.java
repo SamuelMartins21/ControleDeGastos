b61d8a8f6d2d98c3c4a108f3da61b9e37bd39f48
Samuel Martins <122890386+SamuelMartins21@users.noreply.github.com>
2023-01-23 10:31:52 -0300

    Update ControleDeGastosApplication.java

--- Java/controle-de-gastos/src/main/java/com/example/controledegastos/ControleDeGastosApplication.java
+++ Java/controle-de-gastos/src/main/java/com/example/controledegastos/ControleDeGastosApplication.java
@@ -8,7 +8,6 @@ public class ControleDeGastosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ControleDeGastosApplication.class, args);
-		System.out.println("Tudo Funcionando");
 	}
 
 }
