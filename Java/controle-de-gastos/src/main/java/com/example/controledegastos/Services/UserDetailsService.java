93927cc0f68f5cde7edc508629c8285c90fd83fe
Samuel Martins <samuelsilvaa431@gmail.com>
2023-05-20 14:36:36 -0300

    Configurando Users

---
+++ Java/controle-de-gastos/src/main/java/com/example/controledegastos/Services/UserDetailsService.java
@@ -0,0 +1,27 @@
+package com.example.controledegastos.Services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.controledegastos.models.UserModel;
+import com.example.controledegastos.repositories.UserRepository;
+
+@Service
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+
+    final UserRepository userRepository;
+
+    public UserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserModel userModel = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        "Não foi Possível encontrar o nome de usuário: " + username));
+        return userModel;
+    }
+
+}
--- Java/controle-de-gastos/src/main/java/com/example/controledegastos/repositories/UserRepository.java
+++ Java/controle-de-gastos/src/main/java/com/example/controledegastos/repositories/UserRepository.java
@@ -1,5 +1,6 @@
 package com.example.controledegastos.repositories;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.controledegastos.models.UserModel;
 
 public interface UserRepository extends JpaRepository<UserModel, UUID> {
-    
+
+    Optional<UserModel> findByUsername(String username);
 }
