b8a1ef2fccda43b8be8589eaa751c7ee5fe0998e
Samuel Martins <samuelsilvaa431@gmail.com>
2023-05-20 11:31:02 -0300

    Criando WebSecurity

---
+++ Java/controle-de-gastos/src/main/java/com/example/controledegastos/configs/security/WebSecurityConfig.java
@@ -0,0 +1,9 @@
+package com.example.controledegastos.configs.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfiguration {
+    
+}
